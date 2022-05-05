1aab6e8ab18bc9b159aa329dd05dacce712296c4
Greg Wu <mrgrwu@gmail.com>
2022-05-05 12:28:40 -0700

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Greg Wu on 10/26/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
